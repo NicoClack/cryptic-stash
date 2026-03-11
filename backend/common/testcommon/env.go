e4e6dfd975f6133c2efa6e3b4f6a2231d4f1f706
Nico Clack <nico@bn7.net>
2026-03-11 13:55:01 +0000

    Update test env

--- backend/common/testcommon/env.go
+++ backend/common/testcommon/env.go
@@ -31,6 +31,9 @@ func DefaultEnv() *common.Env {
 		JOB_POLL_INTERVAL:    time.Hour * 999,
 		MAX_TOTAL_JOB_WEIGHT: 100,
 
+		SIGNUP_LINK_DEFAULT_EXPIRY: time.Hour * 24 * 2,
+		SIGNUP_LINK_MAX_EXPIRY:     time.Hour * 24 * 7,
+
 		UNLOCK_TIME:                               time.Hour * 24 * 7,
 		AUTH_CODE_VALID_FOR:                       time.Hour * 24 * 3,
 		USED_AUTH_CODE_VALID_FOR:                  time.Hour,
